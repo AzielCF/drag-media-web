cccb320b383a1b5f54788bb70af778204b32f635
Aziel <azielcruzado@gmail.com>
2024-12-02 22:39:29 -0500

    add const vars

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
-export const SITE_TITLE = 'Drag media';
+export const SITE_TITLE = 'Drag Media: Descarga y Arrastra Medios Fácilmente';
 export const SITE_DESCRIPTION = 'Descarga y arrastra imágenes fácilmente con nuestra aplicación de escritorio y funcionalidades potentes.';
+export const SITE_OG_NAME = 'Drag Media';
+export const SITE_OG_DESCRIPTION = 'Herramienta intuitiva para descargar y arrastrar medios de manera simple y rápida.';
 export const GIT_OWNER_LINK = "https://github.com/AzielCF";
 export const GIT_REPOSITORY_LINK = GIT_OWNER_LINK + "/Drag-media";
 export const GIT_REPOSITORY_OWNER = "AzielCF";
